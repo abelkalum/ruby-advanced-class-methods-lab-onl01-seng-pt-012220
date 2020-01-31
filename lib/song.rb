48e63e821c88070cbdac1ccd51cee70945c10f5d
Abel Kalum <chumbabel@gmail.com>
2020-01-31 22:49:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,12 +14,8 @@ class Song
     self.class.all << self
   end
   
-  
-  
-  def self.create(song)
-    song = self.new
-    song.name = name
-    @@all << song
+  def self.create
+    @@all << self.new
   end
 
 end
